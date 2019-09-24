6c2092e8ccc636579b32f3c5c40dc2f5ff75ecb5
Bruno Christian <brunochristian6@gmail.com>
2019-09-24 20:48:37 +0000

    Criando Token JWT

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+require("dotenv/config");
 const { check, validationResult } = require("express-validator");
 
 //@route POST api/users
@@ -44,7 +46,23 @@ router.post(
 
       await user.save();
 
-      res.send("user saved");
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        process.env.JWT_SECRET,
+        {
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (error) {
       console.log(err.message);
       res.status(500).send("Server error");
