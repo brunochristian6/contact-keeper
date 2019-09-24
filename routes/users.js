9434c7810be372071f3bd4488df3416a0b530163
Bruno Christian <brunochristian6@gmail.com>
2019-09-24 20:34:33 +0000

    Configurando Hash password e registrando usuario no banco

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const User = require("../models/User");
-const { check, validationResult } = require("express-validator/check");
+const bcrypt = require("bcryptjs");
+const { check, validationResult } = require("express-validator");
+
 //@route POST api/users
 //@desc  Registrar user
 //@access Public
@@ -16,12 +18,37 @@ router.post(
       "Favor informar uma senha com no minimo 6 characters "
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(404).json({ erros: errors.array() });
     }
-    res.send("passed");
+    const { name, email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({ msg: "User ja cadastrado" });
+      }
+
+      user = new User({
+        name,
+        email,
+        password
+      });
+
+      //HashPassword
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.send("user saved");
+    } catch (error) {
+      console.log(err.message);
+      res.status(500).send("Server error");
+    }
   }
 );
 
