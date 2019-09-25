951291b52983b78c00bdb0bc7e4ae7fc477e1a4e
Bruno Christian <brunochristian6@gmail.com>
2019-09-25 18:56:07 +0000

    Criando componente navbar e configurando rotas com react-router-dom

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>About this App</h1>
+      <p className="my-1">This is a Full stack React APP & NodeJS</p>
+    </div>
+  );
+};
+
+export default About;
