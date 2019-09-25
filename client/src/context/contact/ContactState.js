54f39968908481a699de33ca1d7c627138b4373d
Bruno Christian <brunochristian6@gmail.com>
2019-09-25 19:18:33 +0000

    Configurando Context API

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,47 @@
+import React, { useReducer } from "react";
+import uuid from "uuid";
+import ContactContext from "./contactContext";
+import contactReducer from "./contactReducer";
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER
+} from "../types";
+
+const ContactState = props => {
+  const initialState = {
+    contacts: []
+  };
+
+  const [state, dispatch] = useReducer(contactReducer, initialState);
+
+  //ADD CONTACT
+
+  //DELETE CONTACT
+
+  //SET CURRENT CONTACT
+
+  //CLEAR CURRENT CONTACT
+
+  //UPDATE CONTACT
+
+  //FILTER CONTACTS
+
+  //CLEAR FILTER
+
+  return (
+    <ContactContext.Provider
+      value={{
+        contacts: state.contacts
+      }}
+    >
+      {props.children}
+    </ContactContext.Provider>
+  );
+};
+
+export default ContactState;
