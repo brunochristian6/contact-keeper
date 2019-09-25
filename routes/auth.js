0d99e1589a005d886f2bb18fa300c18be8ce4be1
Bruno Christian <brunochristian6@gmail.com>
2019-09-25 13:15:30 +0000

    Criando middleware de autenticacao de usuario e protegendo rotas privadas

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,76 @@
 const router = require("express").Router();
-
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+require("dotenv/config");
+const { check, validationResult } = require("express-validator");
+const authMiddleware = require("../middleware/auth");
 //@route GET api/auth
 //@desc   Get logged user
 //@access Privado
-router.get("/", (req, res) => {
+router.get("/", authMiddleware, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (error) {
+    console.log(err.message);
+    res.status(500).send("Server error");
+  }
+
   res.json({ msg: "Get Logged in user" });
 });
 
 //@route POST api/auth
 //@desc   Auth user and get token
 //@access Publico
-router.post("/", (req, res) => {
-  res.json({ msg: "Log in user" });
-});
+router.post(
+  "/",
+  [
+    check("email", "Entre com um email valido").isEmail(),
+    check("password", "Favor informar a senha").exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(404).json({ erros: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ msg: "Email ou senha incorretos" });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.status(400).json({ msg: "Email ou senha incorretos" });
+      }
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        process.env.JWT_SECRET,
+        {
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (error) {
+      console.log(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
 
 module.exports = router;
--- routes/contacts.js
+++ routes/contacts.js
@@ -1,30 +1,31 @@
 const router = require("express").Router();
+const authMiddleware = require("../middleware/auth");
 
 //@route GET api/contacts
 //@desc  GET contacts
 //@access Private
-router.get("/", (req, res) => {
+router.get("/", authMiddleware, (req, res) => {
   res.json({ msg: "Get all Contacts" });
 });
 
 //@route POST api/contacts
 //@desc  Add new contacts
 //@access Private
-router.post("/", (req, res) => {
+router.post("/", authMiddleware, (req, res) => {
   res.json({ msg: "Add new Contacts" });
 });
 
 //@route PUT api/contacts/:id
 //@desc  edit contacts
 //@access Private
-router.put("/:id", (req, res) => {
+router.put("/:id", authMiddleware, (req, res) => {
   res.json({ msg: "Update Contacts" });
 });
 
 //@route DELETE api/contacts/:id
 //@desc  Delete contacts
 //@access Private
-router.delete("/:id", (req, res) => {
+router.delete("/:id", authMiddleware, (req, res) => {
   res.json({ msg: "Delete Contacts" });
 });
 
