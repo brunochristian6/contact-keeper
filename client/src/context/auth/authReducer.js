552f1e1322208277710a9b1288e27f54846777d9
Bruno Christian <brunochristian6@gmail.com>
2019-09-27 11:38:53 +0000

    Criando estrutura da Context API para autenticacao de usuarios

---
+++ client/src/context/auth/authReducer.js
@@ -0,0 +1,23 @@
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT,
+  CLEAR_ERRORS
+} from "../types";
+
+export default (state, action) => {
+  switch (action.type) {
+    case ADD_CONTACT:
+      return {
+        ...state,
+        contacts: [...state.contacts, action.payload]
+      };
+
+    default:
+      return state;
+  }
+};
